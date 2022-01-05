eefcadb0b4382f298aca4d9d761c06d65c6391d2
戴海洋 <1037306928@qq.com>
2022-01-05 17:42:00 +0800

    解析html的注释

--- src/core/instance/render-helpers/index.js
+++ src/core/instance/render-helpers/index.js
@@ -12,20 +12,22 @@ import { bindObjectListeners } from './bind-object-listeners'
 import { resolveScopedSlots } from './resolve-scoped-slots'
 import { bindDynamicKeys, prependModifier } from './bind-dynamic-keys'
 
+// render函数中编译后的函数
+// _c 就是执行createElement 去创建 VNode
 export function installRenderHelpers (target: any) {
-  target._o = markOnce
+  target._o = markOnce // 标记渲染一次
   target._n = toNumber
   target._s = toString
-  target._l = renderList
-  target._t = renderSlot
+  target._l = renderList // 渲染列表
+  target._t = renderSlot // 渲染插槽
   target._q = looseEqual
   target._i = looseIndexOf
-  target._m = renderStatic
+  target._m = renderStatic // 渲染静态节点
   target._f = resolveFilter
   target._k = checkKeyCodes
   target._b = bindObjectProps
-  target._v = createTextVNode
-  target._e = createEmptyVNode
+  target._v = createTextVNode // 文本VNode
+  target._e = createEmptyVNode // 空VNode
   target._u = resolveScopedSlots
   target._g = bindObjectListeners
   target._d = bindDynamicKeys
